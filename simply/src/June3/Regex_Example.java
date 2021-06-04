7fbc10c856cc7ec89c88ca6878118c877e948977
Kartik <kartik17157@iiitd.ac.in>
2021-06-04 13:53:36 +0530

    Regex 3rd part is added

--- simply/src/June3/Regex_Example.java
+++ simply/src/June3/Regex_Example.java
@@ -11,7 +11,7 @@ public class Regex_Example {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter an email id ");
 		String email = sc.nextLine();
-		String regex = "[A-Za-z0-9_.+]+@(.+)$";
+		String regex = "[A-Za-z0-9_.+]+@[A-Za-z0-9_.+]+\\.com";
 		Pattern p = Pattern.compile(regex);
 		Matcher c = p.matcher(email);
 		System.out.println(c.matches());
@@ -19,10 +19,19 @@ public class Regex_Example {
 		
 		System.out.println("Please enter a phone number");
 		String phone = sc.nextLine();
-		String regex1 = "[+]91\\d{10}";
+		String regex1 = "[+]91[0-9]{10}";
 		Pattern p1 = Pattern.compile(regex1);
 		Matcher c1 = p1.matcher(phone);
 		System.out.println(c.matches());
+		
+		System.out.println("Please enter a password");
+		String pass = sc.nextLine();
+		String regex2 = "[A-Z]+[a-zA-Z]*[0-9][a-zA-Z]*[0-9][a-zA-Z]*[0-9][a-zA-Z]";
+		Pattern p2 = Pattern.compile(regex2);
+		Matcher c2 = p2.matcher(pass);
+		System.out.println(c.matches());
+		
+		
 		
 		
 	}
