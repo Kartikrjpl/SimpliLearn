7633156b86733263ab53031f943cb20beb4406ed
Kartik <kartik17157@iiitd.ac.in>
2021-07-02 17:35:45 +0530

    file updated

--- Phase 4/Code/Assisted Practice/Day4/lifecycle.js
+++ Phase 4/Code/Assisted Practice/Day4/lifecycle.js
@@ -10,7 +10,7 @@ class Music extends React.Component{
     }
     change = () => {
         this.setState({Instrument:"Drums"});
-    }
+    };
 
     render() {
         return(
@@ -25,3 +25,60 @@ class Music extends React.Component{
 }
 
 ReactDOM.render(<Music />, document.getElementById('root'))
+
+
+
+class Music extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={Instrument: "Guitar"};
+    }
+    componentDidMount(){
+        setTimeout(() => {this.setState({Instrument:"Drums"})},2000)
+    }
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        document.getElementById("CON1").innerHTML = "Before the update the instrument waas " +prevState.Instrument;
+    }
+    componentDidUpdate(){
+        document.getElementById("CON2").innerHTML = "The updated instrument is "+this.state.Instrument;
+    }
+    render(){
+        return(
+            <div>
+                <h1>I know how to play {this.state.Instrument}</h1>
+                <div id = "CON1"> </div>
+                <div id = "CON2"> </div>
+                
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Music />, document.getElementById('root'))
+
+
+
+class Music extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={Instrument: "Guitar"};
+    }
+    componentDidMount(){
+        setTimeout(() => {this.setState({Instrument:"Drums"})},2000)
+    }
+    componentDidUpdate(){
+        document.getElementById("CON").innerHTML = "The updated instrument is "+this.state.Instrument;
+    }
+    render(){
+        return(
+            <div>
+                <h1>I know how to play {this.state.Instrument}</h1>
+                <div id = "CON"> </div>
+                
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Music />, document.getElementById('root'))
+
